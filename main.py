9b6ead48833e386801565482c088c5c14d31a6ce
Daniel <d8ikmetov@yandex.ru>
2025-03-17 11:45:42 +0500

    Version V1.39

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import random
 
-VERSION=1.35 #1.4
+VERSION=1.39
 
 running=True
-
+#TODO: Add language support
 m_procedural={'p','proced','procedural'}
 m_random={'r','ran','rand','random','rng'}
 m_reverse={'e','rev','reverse'}
@@ -13,6 +13,8 @@ a_true={'t','true','1','y','yes','yeah'}
 
 a_exit={'exit','break','ctrl+c','escape','ctrlc'}
 #a_menu={'menu','configuration','home'}
+
+design_line='_______________________________'
 
 mistake_count=0
 
@@ -29,7 +31,7 @@ def isNumber(text):
     if text.isnumeric():return int(text)
     return 0
 
-print('\nSquare Number X\n_______________\n')
+print('\nSquare Number X | Version: '+str(VERSION)+'\n'+design_line+'\n')
 
 minimal=isNumber(exitCheck(input('Enter minimum: ')))
 maximal=isNumber(exitCheck(input('Enter maximum: ')))
@@ -41,38 +43,37 @@ if checkInside(mode,m_random):skip_number_with_zero=exitCheck(input('\nSkip numb
 elif not checkInside(mode,m_remember):error_fix=exitCheck(input('\nError corrector\nEnable: ')).lower()
 else:error_fix=False
 
-print('_______________________________\n')
+print(design_line+'\n')
 
 prevnumber=minimal-1
 
-while running:
-    if checkInside(mode,m_remember):
-        running=False
-        while prevnumber<=maximal-1:
-            prevnumber+=1
-            number=prevnumber
+if checkInside(mode,m_remember):
+    while prevnumber<=maximal-1:
+        prevnumber+=1
+        number=prevnumber
 
-            print(str(number)+'x'+str(number)+': '+str(number**2))
-    else:
-        if checkInside(mode,m_random):
-            number=random.randint(minimal,maximal)
-        elif checkInside(mode,m_reverse):
-            if prevnumber<=minimal:
-                prevnumber=maximal+1
-            prevnumber-=1
-            number=prevnumber
-        else:
-            if prevnumber>=maximal:
-                prevnumber=minimal-1
-            prevnumber+=1
-            number=prevnumber
+        print(str(number)+'x'+str(number)+': '+str(number**2))
+else:
+    while running:
+            if checkInside(mode,m_random):
+                number=random.randint(minimal,maximal)
+            elif checkInside(mode,m_reverse):
+                if prevnumber<=minimal:
+                    prevnumber=maximal+1
+                prevnumber-=1
+                number=prevnumber
+            else:
+                if prevnumber>=maximal:
+                    prevnumber=minimal-1
+                prevnumber+=1
+                number=prevnumber
 
-        correct=number**2
-        answer=exitCheck(input(str(number)+'x'+str(number)+': '))
-        if answer==str(correct):print(' [+] Correct.')
-        else:
-            print(' [-] Wrong. Right answer is '+str(correct)+'.')
-            if checkInside(error_fix,a_true):
-                prevnumber=minimal-1
+            correct=number**2
+            answer=exitCheck(input(str(number)+'x'+str(number)+': '))
+            if answer==str(correct):print(' [+] Correct.')
+            else:
+                print(' [-] Wrong. Right answer is '+str(correct)+'.')
+                if checkInside(error_fix,a_true):
+                    prevnumber=minimal-1
 
-        print('_______________________________\n')
+            print(design_line+'\n')
